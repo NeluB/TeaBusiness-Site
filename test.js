52ac17d4711a379d27405e4e26ebe47ce4079aa3
Neluka B <33115091+NeluB@users.noreply.github.com>
2021-04-29 12:44:38 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+/*var content =document.getElementById("content");
+var button = document.getElementById("show-more");
+
+button.onclick = function(){
+
+    if(content.className =="open"){
+        //shrink button
+        content.className="";
+        button.innerHTML ="show more";
+
+    }else {
+        //exapand the box
+        content.className = "open";
+        button.innerHTML ="show less";
+    }
+};
+*/
